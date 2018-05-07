80341bd5fde6418d2e9a48b5f79c4a0679f6f45a
jcc30 <costacamps8@gmail.com>
2018-05-07 18:10:55 +0200

    ex12 done (not done in class yet)

---
+++ Exercici12/NutritionFacts.java
@@ -0,0 +1,105 @@
+/*
+ * This project has been developed by Josep Antoni Costa Camps
+ */
+package Exercici12;
+
+/**
+ *
+ * @author jcc30
+ */
+public class NutritionFacts {
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
+    public NutritionFacts(NutritionFactsBuilder builder){
+        this.servingSize = builder.servingSize;
+        this.servings = builder.servings;
+        this.calories = builder.calories;
+        this.fat = builder.fat;
+        this.sodium = builder.sodium;
+        this.carbohydrate = builder.carbohydrate;
+    }
+
+    /**
+     * @return the servingSize
+     */
+    public int getServingSize() {
+        return this.servingSize;
+    }
+
+    /**
+     * @return the servings
+     */
+    public int getServings() {
+        return this.servings;
+    }
+
+    /**
+     * @return the calories
+     */
+    public int getCalories() {
+        return this.calories;
+    }
+
+    /**
+     * @return the fat
+     */
+    public int getFat() {
+        return this.fat;
+    }
+
+    /**
+     * @return the sodium
+     */
+    public int getSodium() {
+        return this.sodium;
+    }
+
+    /**
+     * @return the carbohydrate
+     */
+    public int getCarbohydrate() {
+        return this.carbohydrate;
+    }
+    
+    public static class NutritionFactsBuilder{
+        private final int servingSize;
+        private final int servings;
+        private int calories;
+        private int fat;
+        private int sodium;
+        private int carbohydrate;
+        
+        public NutritionFactsBuilder(int servingSize, int servings){
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+        
+        public NutritionFactsBuilder Calories(int calories){
+            this.calories = calories;
+            return this;
+        }
+        
+        public NutritionFactsBuilder Fat(int fat){
+            this.fat = fat;
+            return this;
+        }
+        
+        public NutritionFactsBuilder Sodium(int sodium){
+            this.sodium = sodium;
+            return this;
+        }
+        
+        public NutritionFactsBuilder Carbohydrate(int carbohydrate){
+            this.carbohydrate = carbohydrate;
+            return this;
+        }
+        
+        public NutritionFacts build() {
+            return new NutritionFacts(this);
+        }
+    }
+}
