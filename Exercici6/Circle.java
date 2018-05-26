0f819957e9abedda63a464276f585d3ba37f48b6
David Sarrat <signup@davidsarrat.com>
2018-05-26 13:13:20 +0200

    Update Circle.java

--- Exercici6/Circle.java
+++ Exercici6/Circle.java
@@ -1,28 +1,27 @@
-/*
- * This project has been developed by Josep Antoni Costa Camps
- */
-package Exercici6;
+package ex6;
 
-/**
- *
- * @author jcc30
- */
-public class Circle extends Figure{
+public class Circle extends Figure {
+
+    private final float r;
     
-    private final float z;
-    
-    public Circle(float x, float y, float z) {
+    public Circle(float x, float y, float r){
         super(x, y);
-        this.z = z;
+        this.r = r;
     }
-    
+
     public Circle(Circle c){
         super(c);
-        this.z = c.z;
+        this.r = c.r;
     }
     
     @Override
-    public Circle copy(){
+    public Figure copy() {
+        return this;
+    }
+
+    @Override
+    public Figure deepCopy() {
         return new Circle(this);
     }
+    
 }
